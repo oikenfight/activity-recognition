fddf204bab307b46941d2776c8acee38c6ef4ef2
YutaOikawa <oikenfight@gmail.com>
2018-08-14 19:28:56 +0900

    add check_pkl.py

---
+++ src/cnn/check_pkl.py
@@ -0,0 +1,25 @@
+import pickle as pkl
+from FileManager import FileManager
+
+
+if __name__ == "__main__":
+    BASE = './src/cnn'
+    BASE_DATA = BASE + '/dataset/20180814_100001'
+    # BASE_DATA = BASE + '/dataset/20180814_094004'
+    FileManager.BASE_DIR = BASE_DATA
+    file_manager = FileManager()
+
+    all_files = file_manager.all_files()
+
+    for file in all_files:
+        print('=================================')
+        print(file)
+        with open(file, 'rb') as f:
+            dataset = pkl.load(f)
+
+        for folder_name, features in dataset.items():
+            print('-----------------------------------------')
+            print('folder name: ', str(folder_name))
+            for j, feature in enumerate(features):
+                print('number:', str(j))
+                print(feature)
