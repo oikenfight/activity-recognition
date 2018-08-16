9ebb3f64c76783ec0738e7acf6ca8024fed7e4c3
YutaOikawa <oikenfight@gmail.com>
2018-08-16 15:56:04 +0900

    fix label_data in Dataset

--- src/lstm/Dataset.py
+++ src/lstm/Dataset.py
@@ -18,23 +18,28 @@ class Dataset:
     def load(self):
         print('Dataset Loading ...')
         print('action length: ', str(len(self.all_files)))
-        for pkl_file in self.all_files:
+        for i, pkl_file in enumerate(self.all_files):
+            print('=================================')
+            print(pkl_file)
+
             # actions 辞書作成
-            action_label = len(self.actions)
             action_name = pkl_file.split('.')[0]
             self.actions[len(self.actions)] = action_name
 
+            # 正解ラベル作成
+            action_label = np.zeros(len(self.all_files), dtype=np.int32)
+            action_label[i] = 1
+
+            print(action_label)
+
             with open(pkl_file, 'rb') as f:
                 dataset = pkl.load(f)
+            print('data number: ', str(len(dataset)))
 
             # ラベルと時系列画像特徴量リストが対応するデータセットを作成
             for folder_name, features in dataset.items():
                 np.append(self.label_data, action_label)
                 np.append(self.feature_data, features)
-
-            print('=================================')
-            print(pkl_file)
-            print('data number: ', str(len(dataset)))
 
     def get_data_of(self, index: int) -> tuple:
         return self.label_data[index], self.feature_data[index]
@@ -43,7 +48,7 @@ class Dataset:
         return self.actions[label_index]
 
     def get_label_from_action(self, action_name: str) -> int:
-        return [label_index for label_index, val in self.actions.items() if v == action_name][0]
+        return [label_index for label_index, val in self.actions.items() if val == action_name][0]
 
 
 if __name__ == "__main__":
