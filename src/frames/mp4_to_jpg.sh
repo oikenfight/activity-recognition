c15cb521b90e81e12568c5cfa72c99d314af4e83
YutaOikawa <oikenfight@gmail.com>
2018-08-25 18:18:27 +0900

    update params to get data and output path.

--- src/frames/mp4_to_jpg.sh
+++ src/frames/mp4_to_jpg.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
-EXPECTED_ARGS=2
+EXPECTED_ARGS=5
 E_BADARGS=65
-BASE='./src/frames'
-BASE_DATA=${BASE}'/data'
-BASE_OUTPUT=${BASE}'/frame_data'
 
 if [ $# -lt ${EXPECTED_ARGS} ]
 then
@@ -12,8 +9,10 @@ then
   exit ${E_BADARGS}
 fi
 
-ACTION=$1
-FILE_NAME=$2
-FPS=$3
+BASE_DATA=$1
+BASE_OUTPUT=$2
+ACTION=$3
+FILE_NAME=$4
+FPS=$5
 
 ffmpeg -i ${BASE_DATA}/${ACTION}/${FILE_NAME}.mp4 -f image2 -vf fps=${FPS} ${BASE_OUTPUT}/${ACTION}/${FILE_NAME}/%04d.jpg
