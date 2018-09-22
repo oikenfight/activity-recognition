7abd5f016e45c05f278a222950936c809062c2a8
YutaOikawa <oikenfight@gmail.com>
2018-09-22 17:56:17 +0900

    fix how to import modules, and delete it.

--- src/frames/mp4_convert_batch.py
+++ src/frames/mp4_convert_batch.py
@@ -1,4 +1,9 @@
 import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '.'))
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from FileManager import FileManager
 
 
