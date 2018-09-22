fc8714dcfa0d27213ddfe8b376ac25e1107aa060
YutaOikawa <oikenfight@gmail.com>
2018-09-22 18:10:34 +0900

    fix it.

--- src/cnn/create_cnn_dataset.py
+++ src/cnn/create_cnn_dataset.py
@@ -6,10 +6,10 @@ import os
 import sys
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '.'))
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from image_model import VGG19
 
-from .image_model import VGG19
-from ..FileManager import FileManager
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from FileManager import FileManager
 
 
 class Features:
