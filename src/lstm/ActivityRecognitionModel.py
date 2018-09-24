0da6b46601027a53609632aedfb90b13e622c684
YutaOikawa <oikenfight@gmail.com>
2018-09-24 16:53:48 +0900

    modify ActivityRecognitionModel.

--- src/lstm/ActivityRecognitionModel.py
+++ src/lstm/ActivityRecognitionModel.py
@@ -12,7 +12,6 @@ import time
 
 class ActivityRecognitionModel(chainer.Chain):
     dropout_ratio = 0.5
-    cp = cuda.cupy
 
     def __init__(self, feature_size, hidden_size, out_size):
         """
@@ -26,28 +25,12 @@ class ActivityRecognitionModel(chainer.Chain):
             output=L.Linear(hidden_size, out_size)
         )
 
-    def __call__(self, data, label):
-
-        data = self.cp.asarray(data).astype(np.float32)
-        label = self.cp.asarray(label).astype(np.int32)
-
-        loss = None
-        accuracy = None
+    def __call__(self, features):
         self.lstm.reset_state()
-
-        t = label.astype(np.int32)
-
-        # TODO: ignore_part は src/cnn/create_cnn_dataset.py でミスったため、shape が期待と違うため。余計に1階層ネストしてる。
-        batch_size, ignore_part, feature_length, feature_size = data.shape
+        batch_size, feature_length, feature_size = features.shape
         for col in range(feature_length):
-            x = data[:, 0, col]    # TODO: ここも上のと同じ。0列目を指定して、無駄なネストを無視する感じ。
-            h1 = self.image_vec(x)
+            h1 = self.image_vec(features[:, col])
             h2 = self.lstm(h1)
-            h3 = self.output(h2)
-
-            if col == feature_length - 1:
-                loss = F.softmax_cross_entropy(h3, t)
-                accuracy = F.accuracy(h3, t)
-        return loss, accuracy
+        return self.output(h2)
 
 
