c33dd571faecda2db26aab57aeb660bdbbf1dcde
YutaOikawa <oikenfight@gmail.com>
2018-10-21 17:56:50 +0900

    remove unused row in ActivityRecognitionModel.

--- src/3dcnn_lstm_model/lstm/ActivityRecognitionModel.py
+++ src/3dcnn_lstm_model/lstm/ActivityRecognitionModel.py
@@ -1,13 +1,6 @@
-import numpy as np
 import chainer
-from chainer import cuda, Function, gradient_check, report, training, utils, Variable
-from chainer import datasets, iterators, optimizers, serializers
-from chainer import Link, Chain, ChainList
 import chainer.functions as F
 import chainer.links as L
-from chainer.training import extensions
-from chainer import cuda
-import time
 
 
 class ActivityRecognitionModel(chainer.Chain):
