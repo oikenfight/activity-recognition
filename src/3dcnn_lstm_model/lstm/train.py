c7abb02be905c63ad6a88ce05ca526dd24ccf514
YutaOikawa <oikenfight@gmail.com>
2018-10-25 16:50:56 +0900

    modify train.py.

--- src/3dcnn_lstm_model/lstm/train.py
+++ src/3dcnn_lstm_model/lstm/train.py
@@ -5,6 +5,7 @@ from chainer import cuda, datasets, iterators, optimizers, serializers, function
 import time
 from datetime import datetime
 import os
+from glob import glob
 import sys
 import cupy
 
@@ -17,6 +18,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '.'))
 from ActivityRecognitionModel import ActivityRecognitionModel
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+import img2vec
 from FileManager import FileManager
 
 
@@ -30,22 +32,25 @@ class Train:
     EPOCH_NUM = 1000
     FEATURE_SIZE = 4096
     HIDDEN_SIZE = 512
-    BACH_SIZE = 10
+    BACH_SIZE = 3
     TEST_RATE = 0.25
+    FRAME_SIZE = 8
     OVERLAP_SIZE = 4
 
     def __init__(self):
         self.xp = np
         self.actions = {}    # ラベル:アクション名 索引辞書
-        self.videos = self.xp.array([])     # video ファイルまでのパスを格納（labels と順一致）
-        self.labels = self.xp.array([])     # 各 video の正解ラベルを格納（videos を順一致）
-        self.train_indexes = self.xp.array([])
-        self.test_indexes = self.xp.array([])
+        self.frames = np.array([])     # フレームに区切られたビデオ（時系列画像）のパスデータを格納（labels, images と順同一）
+        self.labels = np.array([])     # 各 video の正解ラベルを格納（frames, images と順同一）
+        self.images = np.empty((0, 8, 270, 360, 3))     # 読み込んだ画像ピクセルデータを格納（frames, labels と順同一）
+        self.train_indexes = np.array([])
+        self.test_indexes = np.array([])
         self.model, self.optimizer, self.save_dir = None, None, None
         self.save_dir = ''
 
     def _prepare(self):
         self._load_data()
+        self._load_images()
         self._shuffle_data()
         self._set_model()
         self._set_optimizer()
@@ -66,13 +71,14 @@ class Train:
         画像
         :return void:
         """
+        print('>>> load data')
         FileManager.BASE_DIR = self.INPUT_IMAGE_DIR
         file_manager = FileManager()
 
         # setup
         current_action = ''
         current_label = 0
-        videos = []
+        frames = []
         labels = []
 
         for path_list in file_manager.all_dir_lists():
@@ -81,25 +87,55 @@ class Train:
             path_list[1]: ビデオ名
             """
             # 必要なパスを取得
-            path = path_list.join('/')
+            path = '/'.join(path_list)
             # アクションが更新されたら状態を変更する
             if not path_list[0] == current_action:
                 self.actions[len(self.actions)] = path_list[0]
                 current_action = path_list[0]
                 current_label = len(self.actions)
-            # 各データを追加
-            videos.append(path)
-            labels.append(current_label)
+
+            # ビデオ内のファイル一覧を取得
+            files = sorted(glob(self.INPUT_IMAGE_DIR + path + "/*.jpg"))
+
+            # フレームを作成して、ラベルと共に追加
+            for i in range(0, len(files), self.OVERLAP_SIZE):
+                frame = files[i:i+self.FRAME_SIZE]
+                # TODO: 微妙に 8 フレームに足りてないデータを無駄にしてるから、できたら直す（学習する時にフレーム数が一定のほうが楽だから今はこうしてる。）
+                if len(frame) < 8:
+                    break
+                # 追加
+                frames.append(frame)
+                labels.append(current_label)
+
+            if len(frames) > 10:
+                break
+
         # インスタンス変数に値をセット
-        self.videos = self.xp.array(videos)
+        self.frames = self.xp.array(frames)
         self.labels = self.xp.array(labels)
+
+    def _load_images(self):
+        print('>>> load images')
+        # インスタンスを準備
+        img2vec_converter_instance = img2vec.converter.Converter()
+        # 画像読み込み
+        print(len(self.frames))
+        for frame in self.frames:
+            images_vec = []
+            for path in frame:
+                print(path)
+                vec = img2vec_converter_instance.main(path)
+                images_vec.append(vec)
+            self.images = np.append(self.images, [images_vec], axis=0)    # list のが計算は速いけど、メモリが心配だから。xp だと GPU 使っちゃうから。
+            # TODO: この段階で出来る下処理はしておこうな。平均引くとか。
+            print(self.images.shape)
 
     def _shuffle_data(self):
         """
         任意の割合でランダムに訓練データとテストデータの tuple を返却する
         :return void:
         """
-        self._print_title('shuffle data to train and test')
+        print('>>> shuffle data to train and test')
 
         # index（データ数配列）をランダムに並び替え、2つにデータを分ける
         test_num = int(len(self.labels) * self.TEST_RATE)
@@ -111,7 +147,6 @@ class Train:
         self.test_indexes = indexes[:test_num]
 
     def _set_model(self) -> ActivityRecognitionModel:
-        # TODO: モデルを作成
         self._print_title('set model:')
         self.model = ActivityRecognitionModel(self.FEATURE_SIZE, self.HIDDEN_SIZE, len(self.actions))
 
@@ -167,9 +202,9 @@ class Train:
         loss, acc = [], []
         batch_num = int(len(self.train_indexes) / self.BACH_SIZE)
 
-        for i, (video_batch, label_batch) in enumerate(self._random_batches(self.train_indexes)):
+        for i, (images_batch, label_batch) in enumerate(self._random_batches(self.train_indexes)):
             self.model.cleargrads()
-            _loss, _acc = self._forward(video_batch, label_batch)
+            _loss, _acc = self._forward(images_batch, label_batch)
             _loss.backward()
             self.optimizer.update()
             loss.append(_loss.data.tolist())
@@ -184,8 +219,8 @@ class Train:
 
     def _test(self):
         loss, acc = [], []
-        for i, (feature_batch, label_batch) in enumerate(self._random_batches(self.test_indexes)):
-            _loss, _acc = self._forward(feature_batch, label_batch, train=False)
+        for i, (images_batch, label_batch) in enumerate(self._random_batches(self.test_indexes)):
+            _loss, _acc = self._forward(images_batch, label_batch, train=False)
             loss.append(_loss.data.tolist())
             acc.append(_acc.data.tolist())
         loss_ave, acc_ave = np.average(loss), np.average(acc)
@@ -200,11 +235,12 @@ class Train:
         np.random.shuffle(indexes)
         for i in range(0, len(indexes), self.BACH_SIZE):
             batch_indexes = indexes[i:i+self.BACH_SIZE]
-            yield (self.videos[batch_indexes], self.labels[batch_indexes])
+            # TODO: ここでバッチ画像を返却する前に、反転とかずらすとか、適当に手を加えたいかな。
+            yield (self.images[batch_indexes], self.labels[batch_indexes])
 
-    def _forward(self, feature_batch: np.ndarray, label_batch: np.ndarray, train=True):
+    def _forward(self, images_batch: np.ndarray, label_batch: np.ndarray, train=True):
         # 0軸 と 1軸を入れ替えて転置
-        _x = feature_batch.transpose((1, 0, 2))
+        _x = images_batch.transpose((1, 0, 2, 3, 4))
         # gpu を使っていれば、cupy に変換される
         x = self.xp.asarray(_x).astype(np.float32)
         t = self.xp.asarray(label_batch).astype(np.int32)
@@ -227,7 +263,7 @@ class Train:
         plt.plot(np.arange(epoch+1), np.array(test_loss))
         plt.xlabel('epochs')
         plt.ylabel('loss')
-        plt.savefig('loss.png')
+        plt.savefig('./output/3dcnn_lstm_model/loss.png')
 
     @staticmethod
     def _acc_plot(epoch, train_acc, test_acc):
@@ -236,7 +272,7 @@ class Train:
         plt.plot(np.arange(epoch+1), np.array(test_acc))
         plt.xlabel('epochs')
         plt.ylabel('acc')
-        plt.savefig('acc.png')
+        plt.savefig('./output/3dcnn_lstm_model/acc.png')
 
     @staticmethod
     def _print_title(s: str):
@@ -252,7 +288,7 @@ class Train:
 if __name__ == '__main__':
     # setup
     Train.GPU_DEVICE = 0
-    Train.INPUT_IMAGE_DIR = '/converted_data/{datetime}/'
+    Train.INPUT_IMAGE_DIR = '/converted_data/20180929_071816/'
     Train.OUTPUT_BASE = './output/3dcnn_lstm_model/models/'
     # params
     # execute
