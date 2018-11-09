1e596d0385ba820f1c17ad972ad4101ead520344
YutaOikawa <oikenfight@gmail.com>
2018-11-09 19:33:37 +0900

    use image prepare function by chainer in img2vec.

--- src/cnn_recognition/img2vec/converter.py
+++ src/cnn_recognition/img2vec/converter.py
@@ -1,5 +1,6 @@
 import numpy as np
 from PIL import Image
+import chainer
 
 
 class Converter:
@@ -7,9 +8,10 @@ class Converter:
     画像を読み込んでベクトルに変換するクラス
     データの構造上 chainer の便利クラス使うとやりづらいので、あえての素の Pillow
     """
-
-    WIDTH = 240
-    HEIGHT = 180
+    # TODO: 余裕があったらノイズ加えるとかやりたいね。
+    # TODO: VGG の学習に使うには 224 x 224 にする必要がある。時間ある時に video2img でサイズやり直す。
+    WIDTH = 224
+    HEIGHT = 224
     CONVERT_TYPE = 'RGB'
 
     def __init__(self):
@@ -18,13 +20,15 @@ class Converter:
     def main(self, path):
         """
         :param str path:
-        :return list: list of (270, 360, 3)   # WIDTH, HEIGHT, CONVERT_TYPE による
+        :return list: list of (self.HEIGHT, self.WIDTH, [b, g, r])
         """
         img = Image.open(path)
-        img = self._transform(img)
-        pixels = list(img.getdata())
-        pixels = [pixels[i * self.WIDTH:(i + 1) * self.WIDTH] for i in range(self.HEIGHT)]
-        return pixels
+        x = chainer.links.model.vision.vgg.prepare(img)
+        return x
+
+        # img = self._transform(img)
+        # pixel = self._to_bgr_pixel(img)
+        # return pixel
 
     def _transform(self, img: Image):
         """
@@ -32,17 +36,42 @@ class Converter:
         :param img:
         :return img:
         """
-        # RGB で試すけど、細かい物体認識じゃないしグレースケールでも十分？
         img = img.convert(self.CONVERT_TYPE)
-        # 画像は 1440 * 1080 で必ず来るはず。。一応
+        # 画像は self.WIDTH * self.HEIGHT で必ず来るはず。。一応
         img = img.resize((self.WIDTH, self.HEIGHT), Image.LANCZOS)
         return img
+
+    def _to_bgr_pixel(self, img: Image):
+        """
+        :param Image img:
+        :return list bgr_pixels:
+        """
+        bgr_pixels = []
+        for y in range(self.HEIGHT):
+            row = []
+            for x in range(self.WIDTH):
+                r, g, b = img.getpixel((x, y))
+
+                r = (r - 123.68) / 255.0
+                g = (g - 116.779) / 255.0
+                b = (b - 103.939) / 255.0
+
+                # TODO: VGG で読むには BGR の順になってないといけない。VGG 使うなら最初から openCV で読もうな。
+                row.append([b, g, r])
+            bgr_pixels.append(row)
+
+        # img = np.asarray(bgr_pixels)
+        # print(img.shape)
+        # img = Image.fromarray(np.uint8(img))
+        # img.save('./test.jpg')
+
+        return bgr_pixels
 
 
 if __name__ == "__main__":
     # setup
     # params
-    path = '/converted_data/20180929_071816/brushing_teeth/a017-0160C/0001.jpg'
+    path = '/converted_data/20181106_043229/brushing_teeth/a017-0116C/0010.jpg'
 
     # execute
     converter_instance = Converter()
